0890889642c2f40b95b6d1076cce2da6c1d232a1
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-08 16:01:32 +0600

    connected with mongodb and tested

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -12,6 +13,14 @@ app.use(express.json());
 // testing server
 app.get("/", (req, res) => {
   res.send("api is running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bmqqztp.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 app.listen(port, () => {
