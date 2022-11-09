56ccf97ab19364e4143bdaf1e0fbb5e6d7c6a209
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-10 03:21:59 +0600

    api for sending data to server and db is created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -28,11 +28,37 @@ async function run() {
       .db("ReviewServices")
       .collection("Services");
 
+    const reviewCollection = client.db("ReviewServices").collection("Review");
+
     // api for homepage
     app.get("/home", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const result = await cursor.limit(3).toArray();
+      res.send(result);
+    });
+
+    // api for all services
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // api for service details for individual service
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      console.log(service);
+      res.send(service);
+    });
+
+    // api for adding users info and review in db
+    app.post("/reviews", async (req, res) => {
+      const user = req.body;
+      const result = await reviewCollection.insertOne(user);
       res.send(result);
     });
   } finally {
